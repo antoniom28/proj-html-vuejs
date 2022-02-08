7ce326d8bb8ce641c828f3a43b025fd43dd62f4e
antoniom28 <antomarc@gmail.com>
2022-02-08 15:59:55 +0100

    main-3 completa

--- src/components/section/FeaturedProduct.vue
+++ src/components/section/FeaturedProduct.vue
@@ -5,8 +5,8 @@
                 <img src="../../assets/img/Main/shaving_butter.png" alt="">
             </div>
             <div class="text">
-                <span>THIS MONTHS FEATURE PRODUCT</span>
-                <h1>Shaving Butter</h1>
+                <p>THIS MONTHS FEATURE PRODUCT</p>
+                <h2>Shaving Butter</h2>
                 <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Sint itaque nemo hic officiis consectetur, est, vero similique
                         accusamus fuga deserunt assumenda inventore deleniti in doloribus
@@ -37,7 +37,50 @@ export default {
 <style lang="scss" scoped>
 @import '../../assets/style/partials/variables.scss';
 
-img{
-    width: 100px;
+.special-product{
+    background-color: $cube_1;
+    border-top: 3px solid $text_color;
+    display: flex;
+    width: 800px;
+    margin: 0 auto;
+    transform: translateY(-50px);
 }
+
+.thumb{
+    background-image: url(../../assets/img/Main/avadabarbers-cta-background.jpg);
+    background-size: cover;
+    width: 30%;
+        position: relative;
+
+    img{
+        width: 80%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+    }
+}
+
+.text{
+    width: 70%;
+    text-align: start;
+    padding: 40px 25px;
+
+    p{
+        color: $text_color;
+        font-size: 0.9em;
+    }
+
+    h2{
+        color: $text_2_color;
+    }
+
+    span{
+        color: $text_3_color;
+        display: inline-block;
+        margin: 20px 0 5px 0;
+    }
+}
+
+
 </style>
